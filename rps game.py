1be6e103e48bf66e8a3c07338058707623f7cb4c
anetekalnina <114050797+anetekalnina@users.noreply.github.com>
2022-10-04 21:10:33 +0300

    history save

--- rps game.py
+++ rps game.py
@@ -1,5 +1,7 @@
 import random
 turns = ['rock', 'paper', 'scissors']
+human_turns = []
+computer_turns = []
 
 while(True):
     human_turn = input('Your turn or type exit: ')
@@ -8,6 +10,10 @@ while(True):
     if human_turn == 'exit':
         print('Thank you for playing!')
         break
+
+    human_turns.append(human_turn)
+    computer_turns.append(computer_turn)
+
     print(f'You: {human_turn} vs. Computer: {computer_turn}')
     if human_turn == computer_turn:
         print ('Draw!')
@@ -19,3 +25,7 @@ while(True):
         print ('You win!')
     else:
         print ('You lose!')
+
+print(f'You have played {len(human_turns)} times')
+print(human_turns)
+print(computer_turns)
